bd5999e412cf07e835a6f2fdd5cbe3a42c97e755
rimathia <rimathia@users.noreply.github.com>
2020-09-13 17:04:54 +0200

    at least roughly working for keeping track of existing card names and back sides

--- src/bin/create_card_data.rs
+++ src/bin/create_card_data.rs
@@ -1,11 +1,11 @@
 use clap::Clap;
-use magichawk::{CardPrinting, ScryfallCard, ScryfallCardNames, SCRYFALL_CARD_NAMES};
+use magichawk::{CardPrinting, ScryfallCardNames};
 use serde_json::{from_reader, Value};
-use std::collections::HashMap;
 use std::fs::File;
 
-/// Process a bulk "Default Cards" file from the Scryfall API into a card data file for magichawk
-/// see https://scryfall.com/docs/api/bulk-data for the bulk data
+/// Process a bulk "Default Cards" file from the Scryfall API into a card data file for magichawk,
+/// see https://scryfall.com/docs/api/bulk-data for the bulk data.
+/// The bulk data list of different card names from the Scryfall API is used to exclude tokens.
 #[derive(Clap)]
 #[clap(version = "0.1")]
 struct Opts {
@@ -18,16 +18,15 @@ struct Opts {
 fn main() {
     let opts: Opts = Opts::parse();
 
-    let nontoken_names: ScryfallCardNames =
-        serde_json::from_reader(reqwest::blocking::get(SCRYFALL_CARD_NAMES).unwrap()).unwrap();
+    let nontoken_names = ScryfallCardNames::from_api_call().unwrap();
     println!(
-        "there are {} card names, the first is {}",
+        "There are {} names, the first is {}",
         nontoken_names.names.len(),
         nontoken_names.names[0]
     );
 
     let f = File::open(&opts.input).unwrap();
-    let outputfile = File::create(opts.output).unwrap();
+    let outputfile = File::create(&opts.output).unwrap();
 
     let default_cards: Vec<serde_json::Map<String, Value>> = from_reader(f).unwrap();
     println!(
@@ -35,30 +34,23 @@ fn main() {
         default_cards.len(),
         &opts.input
     );
-    // println!("{}", serde_json::to_string_pretty(&default_cards).unwrap());
 
-    let mut card_data: HashMap<String, Vec<CardPrinting>> = HashMap::new();
-
-    for default_card in default_cards {
-        let sc = ScryfallCard::from_scryfall_object(default_card);
-        match sc {
-            Some(card) => {
-                println!(
-                    "scryfall card: {}",
-                    serde_json::to_string_pretty(&card).unwrap()
-                );
-                if nontoken_names.names.contains(&card.name) {
-                    println!("found name {}, adding", card.name);
-                    card_data
-                        .entry(card.name)
-                        .or_insert(Vec::new())
-                        .push(card.printing);
-                }
-            }
-
-            None => println!("couldn't parse to ScryfallCard"),
-        }
+    let mut card_data: std::collections::HashMap<String, Vec<CardPrinting>> =
+        std::collections::HashMap::new();
+    for default_card in default_cards.iter() {
+        magichawk::insert_scryfall_object(&mut card_data, &nontoken_names, default_card);
     }
 
     serde_json::to_writer(outputfile, &card_data).unwrap();
+
+    let different_cards: usize = card_data
+        .iter()
+        .map(|(_name, printings)| printings.len())
+        .sum();
+    println!(
+        "there are {} card names and {} (card name, set) combinations in {}",
+        card_data.len(),
+        different_cards,
+        &opts.output
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,96 @@
 #[macro_use]
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate serde_json;
 
-use rocket::{http::ContentType, response::Content, Response, State};
+use image::DynamicImage;
+use itertools::Itertools;
+use rocket::{fairing::AdHoc, http::ContentType, response::Content, Response, State};
 use rocket_contrib::serve::StaticFiles;
+use std::fs::File;
 use std::sync::Mutex;
-use std::time::Duration;
 
-#[get("/decklist?<text>")]
-fn process_decklist(
-    state: State<Mutex<magichawk::ScryfallCache>>,
-    text: String,
+#[get("/create_pdf?<decklist>&<backside>")]
+fn create_pdf(
+    image_cache: State<Mutex<magichawk::ScryfallCache>>,
+    card_data: State<Mutex<magichawk::CardData>>,
+    decklist: String,
+    backside: magichawk::BacksideMode,
 ) -> Response<'static> {
-    match magichawk::decklist_to_pdf(&mut state.lock().unwrap(), &text) {
+    let parsed = magichawk::parse_decklist(&decklist);
+    let lookedup: Vec<magichawk::DecklistEntry> = parsed
+        .iter()
+        .filter_map(|line| {
+            let entry = line.as_entry()?;
+            card_data.lock().unwrap().ensure_contains(&entry.card.name);
+            Some(entry)
+        })
+        .collect();
+    let mut cache = image_cache.lock().unwrap();
+    let uris: Vec<(&magichawk::DecklistEntry, String, Option<String>)> = lookedup
+        .iter()
+        .filter_map(|entry| {
+            let mut uris = card_data.lock().unwrap().get_uris(
+                &entry.card.name,
+                entry.card.set.as_ref().map(|s| s.as_str()),
+            )?;
+            cache.ensure_contains(&uris.0)?;
+            uris.1 = if backside != magichawk::BacksideMode::Zero {
+                match uris.1 {
+                    Some(ref back) => match cache.ensure_contains(back) {
+                        Some(_) => uris.1,
+                        None => None,
+                    },
+                    None => None,
+                }
+            } else {
+                None
+            };
+            Some((entry, uris.0, uris.1))
+        })
+        .collect();
+
+    let mut expanded: Vec<&DynamicImage> = Vec::new();
+    for (entry, front, maybe_back) in uris.iter() {
+        match cache.get(front) {
+            Some(image) => {
+                for _i in 0..entry.multiple {
+                    expanded.push(image);
+                }
+            }
+            None => {}
+        }
+        match maybe_back {
+            Some(back) => match cache.get(back) {
+                Some(image) => match backside {
+                    magichawk::BacksideMode::Zero => {}
+                    magichawk::BacksideMode::One => expanded.push(image),
+                    magichawk::BacksideMode::Matching => {
+                        for _i in 0..entry.multiple {
+                            expanded.push(image);
+                        }
+                    }
+                },
+                None => {}
+            },
+            None => {}
+        }
+    }
+
+    let pdf = magichawk::pages_to_pdf(
+        expanded
+            .into_iter()
+            .batching(|it| magichawk::images_to_page(it)),
+    );
+
+    //Response::build()
+    //    .header(ContentType::HTML)
+    //    .sized_body(Cursor::new(format!(
+    //        "parsed: {:?}\nbackside: {:?}\nlookedup: {:?}\nuris: {:?}",
+    //        parsed, backside, lookedup, uris
+    //    )))
+    //    .finalize()
+    match pdf {
         Some(bytes) => Response::build()
             .header(ContentType::PDF)
             .sized_body(std::io::Cursor::new(bytes))
@@ -31,24 +109,92 @@ fn list_cache(state: State<Mutex<magichawk::ScryfallCache>>) -> Content<String> 
 #[get("/cache/purge?<age_seconds>")]
 fn purge_cache(
     state: State<Mutex<magichawk::ScryfallCache>>,
-    age_seconds: Option<u64>,
+    age_seconds: Option<i64>,
 ) -> Content<String> {
     state
         .lock()
         .unwrap()
-        .purge(age_seconds.map(|s| Duration::from_secs(s)));
+        .purge(age_seconds.map(|s| chrono::Duration::seconds(s)));
     list_cache(state)
 }
 
-fn main() {
-    let cache = Mutex::new(magichawk::ScryfallCache::new());
+#[get("/card_names/full")]
+fn card_names_full(card_data_m: State<Mutex<magichawk::CardData>>) -> Content<String> {
+    let card_names = &card_data_m.lock().unwrap().card_names;
+    Content(
+        ContentType::JSON,
+        serde_json::to_string(card_names).unwrap(),
+    )
+}
 
+#[get("/card_names/short")]
+fn card_names_short(card_data_m: State<Mutex<magichawk::CardData>>) -> Content<String> {
+    let names = &card_data_m.lock().unwrap().card_names.names;
+    Content(
+        ContentType::HTML,
+        format!(
+            "There are {} card names, the first three are {:?}, {:?}, {:?}",
+            names.len(),
+            names.get(0),
+            names.get(1),
+            names.get(2),
+        ),
+    )
+}
+
+#[get("/lookup")]
+fn lookup(card_data_m: State<Mutex<magichawk::CardData>>) -> Content<String> {
+    let lookup = &card_data_m.lock().unwrap().lookup;
+    Content(ContentType::HTML, format!("{:?}", lookup))
+}
+
+#[get("/card_data/short")]
+fn card_data_short(card_data_m: State<Mutex<magichawk::CardData>>) -> Content<String> {
+    let card_data = card_data_m.lock().unwrap();
+    Content(
+        ContentType::HTML,
+        format!(
+            "There are {} different card names and {} (card name, set) combinations",
+            card_data.printings.len(),
+            card_data
+                .printings
+                .iter()
+                .map(|(_name, printings)| printings.len())
+                .sum::<usize>()
+        ),
+    )
+}
+
+#[get("/card_data/full")]
+fn card_data_full(card_data_m: State<Mutex<magichawk::CardData>>) -> Content<String> {
+    let card_data = card_data_m.lock().unwrap();
+    Content(
+        ContentType::JSON,
+        serde_json::to_string(&card_data.printings).unwrap(),
+    )
+}
+fn main() {
     magichawk::setup_logger().unwrap();
     rocket::ignite()
+        .attach(AdHoc::on_attach("load card data from file", |rocket| {
+            let bulk: std::collections::HashMap<String, Vec<magichawk::CardPrinting>> =
+                serde_json::from_reader(
+                    File::open(rocket.config().get_str("card_data").unwrap()).unwrap(),
+                )
+                .unwrap();
+            Ok(rocket.manage(Mutex::new(magichawk::CardData::from_bulk(bulk).unwrap())))
+        }))
+        .attach(AdHoc::on_attach("create image cache", |rocket| {
+            Ok(rocket.manage(Mutex::new(magichawk::ScryfallCache::new())))
+        }))
         .mount("/", StaticFiles::from("static/"))
-        .mount("/", routes![process_decklist])
+        .mount("/", routes![card_names_full])
+        .mount("/", routes![card_names_short])
+        .mount("/", routes![card_data_full])
+        .mount("/", routes![card_data_short])
+        .mount("/", routes![lookup])
+        .mount("/", routes![create_pdf])
         .mount("/", routes![list_cache])
         .mount("/", routes![purge_cache])
-        .manage(cache)
         .launch();
 }
